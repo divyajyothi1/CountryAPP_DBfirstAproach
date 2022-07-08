b633d3af7622676d82ea9da36dadc3c9a5cb69e2
divya <divya@LAPTOP-90A60C8O>
2022-07-08 12:04:37 -0500

    Add project files.

---
+++ CountryAPP_DBfirstAproach/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CountryAPP_DBfirstAproach.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
